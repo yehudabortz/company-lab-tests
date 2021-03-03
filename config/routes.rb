e1343c4f61b6e17262a8bc4782ae6e6cc527f3fb
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-03 15:19:26 -0600

    Add Sessions

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,11 @@ Rails.application.routes.draw do
   resources :tests
   resources :labs
   resources :companies
+
+
+  resources :sessions, only: [:create, :new, :destroy]
+  get '/signup', to: 'users#new'
+  get '/logout', to: 'sessions#destroy'
+  get '/login', to: 'sessions#new'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
