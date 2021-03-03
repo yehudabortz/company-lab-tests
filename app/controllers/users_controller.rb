7aea4324ebf9e0213bcb8510d80da06bfae6a1ff
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-03 15:54:36 -0600

    Add user show and current_user helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,12 +5,20 @@ class UsersController < ApplicationController
     end
 
     def create
-        raise params.inspect
+        @user = User.new(user_params)
+
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to @user
+        else
+            render "new"
+        end
+
     end
 
     private 
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name)
+        params.require(:user).permit(:first_name, :last_name, :password, :email)
     end
 end
