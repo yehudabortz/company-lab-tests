3776d2b1a3c7ccb82e7cbdd6de5599e6b195993c
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-03 16:46:07 -0600

    Add restful actions

--- app/controllers/labs_controller.rb
+++ app/controllers/labs_controller.rb
@@ -1,2 +1,23 @@
 class LabsController < ApplicationController
+
+    def new
+    end
+
+    def create
+    end
+
+    def index
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
 end
