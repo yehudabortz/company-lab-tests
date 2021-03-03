bceddda787c23faff941cedf911adcaec90bf559
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-03 15:17:13 -0600

    Removed Admin files

--- app/models/admin.rb
+++
@@ -1,6 +0,0 @@
-class Admin < ApplicationRecord
-    has_secure_password
-    
-    belongs_to :company
-    belongs_to :lab
-end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :users
   resources :tests
   resources :labs
-  resources :admins
   resources :companies
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
