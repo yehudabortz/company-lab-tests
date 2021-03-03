55b85282774b754b511af114719df6bccf441c15
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-03 17:48:16 -0600

    Add navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/logout', to: 'sessions#destroy'
   get '/login', to: 'sessions#new'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
