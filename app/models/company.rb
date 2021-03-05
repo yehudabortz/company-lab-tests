60c15a66d2eddba309c2c3b68cf83ed05a5243bc
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-05 14:19:19 -0600

    Removed redundent has many relationship

--- app/models/company.rb
+++ app/models/company.rb
@@ -2,7 +2,6 @@ class Company < ApplicationRecord
     has_many :users
     has_many :tests
     has_many :labs, through: :tests
-    has_many :users, through: :tests
 
     validates :name, uniqueness: { case_sensitive: false }
 
