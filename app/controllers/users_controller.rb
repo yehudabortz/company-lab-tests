d492be77650ba7fc3465f58f68581d3c5091e94d
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-05 15:20:20 -0600

    Update user strong params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
     private 
 
     def user_params
-        params.require(:user).permit(:first_name, :last_name, :password, :email)
+        params.require(:user).permit(:first_name, :last_name, :password, :email, :birthdate, :gender, :phone, :dr_email)
     end
 
 end
