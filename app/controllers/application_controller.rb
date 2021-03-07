6d39c609c016418ff10afc652504ff3fe1d64fab
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-06 19:35:32 -0600

    User method for determining if neccesary info outside of login info has been provided

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,8 +39,5 @@ class ApplicationController < ActionController::Base
       params[:id].to_i
    end
 
-   def customer_has_provided_all_info
-      current_user.first_name != nil && current_user.last_name != nil && current_user.email != nil && current_user.birthdate != nil && current_user.gender!= nil && current_user.phone != nil
-   end
 end
 
