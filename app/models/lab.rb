1ff4119b513af007bd5aabe58acaae5e3d63591a
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-07 11:50:21 -0600

    Add all accepted lab company connections method

--- app/models/lab.rb
+++ app/models/lab.rb
@@ -3,4 +3,9 @@ class Lab < ApplicationRecord
     has_many :tests
     has_many :company_lab_connections
     has_many :companies, through: :company_lab_connections
+
+
+    def self.accepted_company_connections(current_lab)
+        current_lab.company_lab_connections.where(accepted: true)
+    end
 end
