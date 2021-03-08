00b45b91038994cebb9f338567614e10661312f3
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-08 15:21:49 -0600

    remove password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
     validates :first_name, :last_name, :email, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
-    validate :password_requirements_are_met
+    # validate :password_requirements_are_met
 
 
     scope :is_customer, -> {where(is_customer: true)}
@@ -28,15 +28,15 @@ class User < ApplicationRecord
         end
     end
 
-    def password_requirements_are_met
-        rules = {
-          " must contain at least one lowercase letter"  => /[a-z]+/,
-          " must contain at least one uppercase letter"  => /[A-Z]+/,
-          " must contain at least one digit"             => /\d+/,
-        }
+    # def password_requirements_are_met
+    #     rules = {
+    #       " must contain at least one lowercase letter"  => /[a-z]+/,
+    #       " must contain at least one uppercase letter"  => /[A-Z]+/,
+    #       " must contain at least one digit"             => /\d+/,
+    #     }
       
-        rules.each do |message, regex|
-          errors.add( :password, message ) unless password.match( regex )
-        end
-      end
+    #     rules.each do |message, regex|
+    #       errors.add( :password, message ) unless password.match( regex )
+    #     end
+    #   end
 end
