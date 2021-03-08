2fc26cabcdc5d240540abe3be1cc7d4a0d26d965
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-08 13:53:12 -0600

    Add password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
     validates :first_name, :last_name, :email, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
 
+    validate :password_requirements_are_met
 
 
     scope :is_customer, -> {where(is_customer: true)}
@@ -26,4 +27,16 @@ class User < ApplicationRecord
             u.password = SecureRandom.hex(16)
         end
     end
+
+    def password_requirements_are_met
+        rules = {
+          " must contain at least one lowercase letter"  => /[a-z]+/,
+          " must contain at least one uppercase letter"  => /[A-Z]+/,
+          " must contain at least one digit"             => /\d+/,
+        }
+      
+        rules.each do |message, regex|
+          errors.add( :password, message ) unless password.match( regex )
+        end
+      end
 end
