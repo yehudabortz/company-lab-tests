cba3481201c1ccc6fb11a70c3489e3526bbf1a7f
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-08 09:38:26 -0600

    Css helper for input errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
     def readable_timestamp(stamp)
         stamp.strftime("%B %d, %Y at %l:%M %p")
     end
+
+    def format_error(error)
+        error.join(" ")
+    end
+
+    def input_with_errors_class(model, param)
+        "input #{ 'is-danger' if model.errors["#{param}"].any?}"
+    end
 end
