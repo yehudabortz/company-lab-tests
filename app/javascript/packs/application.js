5311969d9f08d27e6398d2c10c9337ed784d76e1
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-09 23:13:39 -0600

    Remove chartkick and Groupdate

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,9 +8,6 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-require("chartkick")
-require("chart.js")
-
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
