73b6fc1ddebcaa900598863024b5c492ae8a519c
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-10 11:38:05 -0600

    Remove comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,12 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :tests
-    # has_many :companies
 
     belongs_to :company, optional: true
     belongs_to :lab, optional: true
     
     validates :first_name, :last_name, :email, presence: true
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP } 
-
-    # validate :password_requirements_are_met
 
 
     scope :is_customer, -> {where(is_customer: true)}
@@ -54,15 +51,4 @@ class User < ApplicationRecord
         end
     end
 
-    # def password_requirements_are_met
-    #     rules = {
-    #       " must contain at least one lowercase letter"  => /[a-z]+/,
-    #       " must contain at least one uppercase letter"  => /[A-Z]+/,
-    #       " must contain at least one digit"             => /\d+/,
-    #     }
-      
-    #     rules.each do |message, regex|
-    #       errors.add( :password, message ) unless password.match( regex )
-    #     end
-    #   end
 end
