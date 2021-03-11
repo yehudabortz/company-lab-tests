b4c579415e7044c47d9bde1d1be97b39baf7959f
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-11 09:19:24 -0600

    Remove unused method

--- app/helpers/tests_helper.rb
+++ app/helpers/tests_helper.rb
@@ -6,9 +6,4 @@ module TestsHelper
             link_to test.unique_test_id, user_test_path(current_user, test)
         end
     end
-
-    def require_test_ownership(test_verifiying_method)
-        redirect_to admin_tests_path, notice: "Forbidden" unless test_verifiying_method
-    end
-    
 end
