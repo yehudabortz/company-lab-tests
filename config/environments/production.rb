6b94066ac341fd637bf787220eaf5f5d2f4c1111
Yehuda Bortz <yehudabortz@icloud.com>
2021-03-11 15:17:58 -0600

    Config default URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-
+  config.action_mailer.default_url_options = { host: 'company-lab-tests.herokuapp.com' }
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
