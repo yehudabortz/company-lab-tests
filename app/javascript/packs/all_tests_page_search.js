e58ced1b91ac9cfc90dd5a67e8fa8bcf6620da06
Yehuda Bortz <yehudabortz@icloud.com>
2021-04-11 22:21:55 -0500

    Remove console.log

--- app/javascript/packs/all_tests_page_search.js
+++ app/javascript/packs/all_tests_page_search.js
@@ -3,13 +3,11 @@ document.addEventListener("turbolinks:load", function () {
 });
 
 function pageSearch() {
-  console.log("hi");
   let input = document.getElementById("page-search");
   let tableRows = document.getElementsByClassName("divide-y divide-gray-200");
   input.addEventListener("keyup", () => {
     let filter = input.value.toUpperCase();
     for (let i = 0; i < tableRows.length; i++) {
-      console.log(tableRows[i]);
       if (tableRows[i].textContent.toUpperCase().indexOf(filter) > -1) {
         tableRows[i].classList.remove("hidden");
       } else {
